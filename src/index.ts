a09a35d684fe71d2c6fae16caf61d2e535e5824a
zr_code <zl18180276692@outlook.com>
2023-07-19 11:31:18 +0800

    feat:localstorage aes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { isNumber } from "@zrcode/jstool"
+// import { isNumber } from "@zrcode/jstool"
 // import { isNumber } from "get-zrtool/dist/js/main.js"
-console.log(isNumber(null))
-console.log(21212)
+import { createLocalStorage } from "./package/storage";
+console.log(createLocalStorage);
+const { set } = createLocalStorage<{
+  name: string;
+  list: Array<number>;
+}>();
+set("name", "zhangsan");
